708adfba0727054258897e87e2ead50894381f86
WigiTech <accounts@wigitech.com>
2018-12-03 21:54:48 +0200

    init

---
+++ app/src/main/java/com/hiday/mygallery/PhotosActivity.java
@@ -0,0 +1,22 @@
+package com.hiday.mygallery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+public class PhotosActivity extends AppCompatActivity {
+    int int_position;
+    private GridView gridView;
+    GridViewAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView)findViewById(R.id.gv_folder);
+        int_position = getIntent().getIntExtra("value", 0);
+        adapter = new GridViewAdapter(this,MainActivity.al_images,int_position);
+        gridView.setAdapter(adapter);
+    }
+}
